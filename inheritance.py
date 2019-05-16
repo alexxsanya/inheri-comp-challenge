9615565cc3cc8272502659727054e10b3c1d3ebe
Alex Ssanya <alexssanya@Alexs-MacBook-Pro.local>
2019-05-16 18:52:10 +0300

    add dog class

--- inheritance.py
+++ inheritance.py
@@ -5,3 +5,15 @@ class Animal:
 
     def make_sound(self):
         return self.sound
+
+class Cow(Animal):
+    def __init__(self,c_type,sex):
+        self.c_type = c_type
+        self.sex = sex
+        Animal.__init__(self,anim_type="herbivores",sound="Moooooow")
+
+
+class Dog(Animal):
+    def __init__(self,name,color):
+        self.name = name
+        Animal.__init__(self,anim_type="carnivores",sound="Boo boo")
